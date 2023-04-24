b5ad847873d01d79101ab6f35167b30edb36730f
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2023-04-24 10:25:40 +0900

    Temporary Commit

--- src/views/ProgressDialog.tsx
+++ src/views/ProgressDialog.tsx
@@ -8,7 +8,7 @@ export function AppProgressDialog() {
   return (
     <Dialog open={context.progress != null}>
       <DialogContent>
-        <Box sx={{ width: "288px" }}>
+        <Box sx={{ width: "256px" }}>
           <LinearProgress variant="determinate" value={(context.progress ?? 0) * 100} />
         </Box>
       </DialogContent>
