683e69f53f81aa18f436d41c08b2cdd9a5659724
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2023-04-26 03:52:37 +0900

    Revert accent color.

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,5 +1,5 @@
 import { Palette, Theme, createTheme } from "@mui/material";
-import { pink, teal } from "@mui/material/colors";
+import { teal } from "@mui/material/colors";
 import { PropsWithChildren, createContext, useEffect, useState } from "react";
 import AppGlobal from "./AppGlobal";
 
@@ -10,7 +10,7 @@ const defaultTheme = createTheme({
       main: teal[300],
     },
     secondary: {
-      main: pink["A200"],
+      main: teal["A200"],
     },
     action: {
       selectedOpacity: 0.84,
