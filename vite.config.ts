a71c1c770b3a9e65795f5d9b832d58ae0e8af2fa
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2024-02-02 20:15:56 +0900

    Make this application work as a Web App.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,wav}']
+      },
+    }),
+  ],
 })
